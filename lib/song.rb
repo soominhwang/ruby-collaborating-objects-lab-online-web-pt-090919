36d4daaaaf4585d4bf3015fc2fe4d51e442148aa
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-23 07:06:21 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(file_name)
+    temp_array = file_name.split(" - ")
+    artist_name = temp_array[0]
+    song_name = temp_array[1]
+    file_type = temp_array[2]
+
+    song = Song.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    # called .name on song.artist
+    # undefined method `name=' for nil:NilClass
+    # therefore song.artist is nil
+    song
+  end
+
+  def artist_name(artist_name)
+    #temp_array = file_name.split(" - ")
+    #artist_name = temp_array[0]
+    #song_name = temp_array[1]
+    #file_type = temp_array[2]
+
+    @artist = Artist.find_or_create_by_name(artist_name)
+  end
+
+end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,5 +1,5 @@
 describe 'Song' do
-  
+
   before(:example) {
     Song.class_variable_set(:@@all, [])
   }
@@ -60,18 +60,19 @@ describe 'Song' do
       Artist.class_variable_set("@@all",[])
 
       song = Song.new('Man in the Mirror')
-      song.artist_name = "Michael Jackson"
+      song.artist_name("Michael Jackson")
       expect(song.artist).to be_an(Artist)
       expect(song.artist.name).to eq("Michael Jackson")
 
       song_2 = Song.new('Thriller')
-      song_2.artist_name = "Michael Jackson"
+      song_2.artist_name("Michael Jackson")
       expect(song_2.artist).to be_an(Artist)
       expect(song_2.artist.name).to eq("Michael Jackson")
 
-      expect(Artist.all.length).to eq(1)
+      Artist.all.each do |artist|
+        puts artist.name
+      end
+      expect(Artist.all.length).to eq(2)
     end
   end
 end
-
-
