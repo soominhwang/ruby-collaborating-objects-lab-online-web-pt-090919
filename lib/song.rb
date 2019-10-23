43dbc5e016271382dc291498fe7729045e9ed7b5
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-23 06:03:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
 
   def self.new_by_filename(name)
     song = Song.new(name)
-    song.name = name.split(" - ")[1]
+    #song.name = name.split(" - ")[1]
     song.artist.name = name.split(" - ")[0]
   end
 
