8edee067693ca5a3eb7aa42530d1aee297eea753
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-23 06:56:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,13 @@
 class MP3Importer
+ attr_accessor :path
 
-  def initialize
-  end
+ def initialize(path)
+  @path = path
+ end
 
-  def files
-  end
-
-  def import
-  end
+ def files
+  Dir.chdir(@path)
+  filename = Dir.glob("*.mp3")
+  filename
+ end
 end
